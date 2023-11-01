13c7d7fcf7853837d062bc0031c7b98ab509601d
itohara22 <ito.k23018@gmail.com>
2023-11-01 12:59:37 +0530

    edited hero section

--- app/page.tsx
+++ app/page.tsx
@@ -5,10 +5,10 @@ export default function Home() {
     <div className="min-h-[calc(100vh-4rem)] grid grid-flow-col place-items-center">
       <div className=" md:block bg-[url('../public/home.jpg')] bg-cover bg-top  h-full w-full md:w-1/2 absolute top-0 md:right-0 z-[-2]"></div>
       <div className="md:mr-[50%]">
-        <h1 className="text-8xl leading-[150px] font-serif font-bold md:text-7xl lg:text-8xl text-center md:leading-[100px] lg:leading-[150px] mr-4 text-white md:text-text drop-shadow-[5px_8px_2px_rgba(0,0,0,0.8)] md:drop-shadow-none">
+        <h1 className="text-8xl leading-[150px] font-serif font-bold md:text-7xl lg:text-8xl text-center md:leading-[100px] lg:leading-[150px] mr-4 text-white md:text-text drop-shadow-[5px_5px_2px_rgba(0,0,0,0.8)] md:drop-shadow-none">
           Find your Dream{" "}
-          <span className="text-primary">
-            <Link href={"/"}>Hotel</Link>
+          <span className="text-primary hover:text-cyan-500 transition-all">
+            <Link href={"/hotels"}>Hotel</Link>
           </span>
         </h1>
       </div>
